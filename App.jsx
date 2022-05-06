1611f349fa5dd61894a0fe20e470c98559a27939
ailtmy <40594834+ailtmy@users.noreply.github.com>
2022-05-06 22:55:57 +0900

    create custome icon

--- App.jsx
+++ App.jsx
@@ -3,5 +3,6 @@ import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 import MemoListScreen from "./src/screens/MemoListScreen";
 
 export default function App() {
+	// return <MemoDetailScreen />;
 	return <MemoListScreen />;
 }
